df7f95a1c83008be801d2df84a5575ed8ed50ab1
= <=>
2022-06-18 12:59:00 +0900

    Rotating

--- art1/index.js
+++ art1/index.js
@@ -1,3 +1,4 @@
+// Canvas size setup START
 const canvas = document.querySelector("canvas");
 
 const resize = () => {
@@ -8,17 +9,59 @@ const resize = () => {
 resize();
 window.addEventListener("resize", resize);
 
-var ctx = canvas.getContext("2d");
+// Canvas size setup END
 
-async function loadImage(src) {
-  let img = new Image();
-  await new Promise((r) => (img.onload = r), (img.src = src));
-  return img;
+const bee = new Image();
+const bottle = new Image();
+const fly = new Image();
+
+function init() {
+  bee.src = "Bee.png";
+  bottle.src = "Bottle.png";
+  fly.src = "Fly.png";
+  window.requestAnimationFrame(draw);
 }
 
-(async () => {
-  bottleImg = await loadImage("Bottle.png");
-  flyImg = await loadImage("Fly.png");
-  beeImg = await loadImage("Bee.png");
-  ctx.drawImage(beeImg, 20, 20, 100, 100);
-})();
+function draw() {
+  const ctx = canvas.getContext("2d");
+
+  ctx.globalCompositeOperation = "destination-over";
+  ctx.clearRect(0, 0, canvas.width, canvas.height); // clear canvas
+
+  ctx.fillStyle = "rgba(0, 0, 0, 0.4)";
+  ctx.strokeStyle = "rgba(0, 153, 255, 0.4)";
+  ctx.save();
+  ctx.translate(150, 150);
+
+  // Earth
+  const time = new Date();
+  ctx.rotate(
+    ((2 * Math.PI) / 60) * time.getSeconds() +
+      ((2 * Math.PI) / 60000) * time.getMilliseconds()
+  );
+  ctx.translate(105, 0);
+  ctx.fillRect(0, -12, 40, 24); // Shadow
+  ctx.drawImage(bee, -12, -12);
+
+  // Moon
+  ctx.save();
+  ctx.rotate(
+    ((2 * Math.PI) / 6) * time.getSeconds() +
+      ((2 * Math.PI) / 6000) * time.getMilliseconds()
+  );
+  ctx.translate(0, 28.5);
+  ctx.drawImage(fly, -3.5, -3.5);
+  ctx.restore();
+
+  ctx.restore();
+
+  ctx.beginPath();
+  ctx.arc(150, 150, 105, 0, Math.PI * 2, false); // Earth orbit
+  ctx.stroke();
+
+  ctx.drawImage(bottle, 0, 0, 300, 300);
+
+  window.requestAnimationFrame(draw);
+}
+
+init();
